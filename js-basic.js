47979b60c5ee20f61e0a53dcdf1520e436c056f5
dhdudtjr <youngsuk50@gmail.com>
2023-11-28 17:20:51 +0900

    온보딩 정리내용

---
+++ js-basic.js
@@ -0,0 +1,50 @@
+// //1. 변수 할당 재선언 데이터 타입 기본 이론 설명
+// var name = "오영석"; // 문자열
+// var age = 30; // 정수
+// var remain = 0.5; // 실수 
+// var bool = false; // 참과 거짓 자료형
+
+// //js에서 출력문
+// console.log(name);
+
+// const 변하지 않는 것 > 상수
+// let 변하는 값 > 변수
+
+// tip: 처음에 const를 모두 사용하고 변수가 있으면 let으로 변경
+
+// 1.변수는 문자와 숫자,$와 _만 사용
+// 2.첫글자는 숫자가 될 수 없습니다.
+// 3.예약어는 사용할 수 없습니다.
+// 4.가급적 상수는 대문자로 알려주세요.
+// 5.변수명은 읽기 쉽고 이해할 수 있게 선언
+
+// 문자열 "" / '' / ``(백틱)
+
+// NaN / not a Number 에러 중요!
+
+// const age = 30;
+// const PI = 3.14;
+
+// console.log(1+2);
+// console.log(10-3);
+// console.log(3*2);
+// console.log(8/ㄴ2);
+// console.log(6%4); // //나머지
+
+
+// const name = "Mike";
+
+// console.log(typeof 3);
+// console.log(typeof name;
+// console.log(typeof true);
+// console.log(typeof "xxx");
+// console.log(typeof null);
+// console.log(typeof undefined);
+
+// alert   알려줌
+// prompt  입력받음
+// confirm 확인받음
+
+// const name = prompt("faf");
+// console.log(name);
+
